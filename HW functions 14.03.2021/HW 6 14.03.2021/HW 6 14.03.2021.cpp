7a55d6707a0a844e046bcf48195d340428235055
D-O-A <79813608+D-O-A@users.noreply.github.com>
2021-03-15 00:39:09 +0200

    Added task 6

--- HW functions 14.03.2021/HW 6 14.03.2021/HW 6 14.03.2021.cpp
+++ HW functions 14.03.2021/HW 6 14.03.2021/HW 6 14.03.2021.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+bool UpperRegister(char symbol)
+{
+	char letterEnd2 = 123, letterEnd1 = 91 , letterStart1 = 64, letterStart2 = 96;   
+	if ((symbol > letterStart1 && symbol < letterEnd1) || (symbol > letterStart2 && symbol < letterEnd2))
+		return true;
+	else
+		return false;
+}
+
 int main()
 {
+	cout << "6.\n\n";
 
+	//Написать логическую функцию, которая получает символьный параметр и возвращает true,
+	//если указанный символ является буквой любого регистра, и false в противном случае.
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Enter a letter:\n";
+	char letter;
+	cin >> letter;
+
+	if (UpperRegister(letter))
+		cout << "Это символ является буквой.\n";
+	else
+		cout << "Этот символ не является буквой.\n";
 }
