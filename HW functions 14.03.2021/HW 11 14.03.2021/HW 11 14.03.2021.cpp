c8db47bfaf53dbb2884e664c58acd875873ca690
D-O-A <79813608+D-O-A@users.noreply.github.com>
2021-03-15 13:29:50 +0200

    Added task 11

---
+++ HW functions 14.03.2021/HW 11 14.03.2021/HW 11 14.03.2021.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int Factorial(int num)
+{
+	int f = 1;
+	for (int i = 1; i <= num ; i++)
+		f *= i;
+	return f;
+}
+
+int main()
+{
+	cout << "11.\n\n";
+	//Написать функцию, вычисляющую факториал заданного значения.
+	setlocale(LC_ALL, "Russian");
+	int num;
+	cout << "Введите число:"; cin >> num;
+	cout << "Факториал заданного числа = " << Factorial(num) << endl;
+}
