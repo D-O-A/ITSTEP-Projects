9715ab4634ba87924d5360d0584f64b8d3e50665
D-O-A <79813608+D-O-A@users.noreply.github.com>
2021-03-19 01:09:30 +0200

    Added task 2 (WiP)

---
+++ HW functions 2 18.03.2021/HW 2 18.03.2021/HW 2 18.03.2021.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int RiddleMeThis()
+{
+	const int SIZE = 4;
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 9 + 1;
+	}
+	cout << endl;
+
+	for (int n = 0; n < SIZE; n++)   // алгоритм отсеивания повторяющихся чисел
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			for (int i = j + 1; i < SIZE; i++)
+			{
+				if (arr[i] == arr[j])
+				{
+					arr[i] = rand() % 9 + 1;
+				}
+			}
+		}
+	}
+
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		return arr[i];
+	}
+}
+
+void PrintArray()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		cout << RiddleMeThis();
+	}
+}
+
+
+
+
+int main()
+{
+	cout << "2.\n\n";
+
+	//Переоформить игру "Быки и коровы" в соответствии с парадигмой процедурного программирования.
+
+	setlocale(LC_ALL, "Russian");
+	srand((unsigned)time(0));
+	
+	RiddleMeThis();
+
+	PrintArray();
+}
