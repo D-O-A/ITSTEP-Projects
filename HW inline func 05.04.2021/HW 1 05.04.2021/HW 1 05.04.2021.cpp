e62a18705dba24726c02a9af841bf8f3cb470bc5
D-O-A <79813608+D-O-A@users.noreply.github.com>
2021-04-05 18:15:32 +0300

    Added inline functions task

---
+++ HW inline func 05.04.2021/HW 1 05.04.2021/HW 1 05.04.2021.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+// я не придумал как можно еще проще написать "приемлемую" для компилятора inline функцию 
+inline bool IsMin(int num1, int num2)
+{
+	return num1 > num2;
+}
+
+int main()
+{
+	cout << "1.\n\n";
+
+	//Написать встраиваемые функции, которые определяют минимум и максимум двух целых чисел.
+
+	setlocale(LC_ALL, "Russian");
+
+	int num1, num2;
+
+	cout << "Введите 2 числа:\n"; cin >> num1 >> num2;
+	cout << endl;
+
+	cout << "Минимум двух чисел = " << (IsMin(num1, num2) ? num2 : num1) << endl
+		 << "Максимум двух чисел = " << (IsMin(num1, num2) ? num1 : num2) << endl;
+}
