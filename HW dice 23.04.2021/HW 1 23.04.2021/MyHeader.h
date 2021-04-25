cdbd92b5f04ed1ed5d3985ba0f8c174a4577ad37
D-O-A <79813608+D-O-A@users.noreply.github.com>
2021-04-25 03:32:06 +0300

    Added dice task

---
+++ HW dice 23.04.2021/HW 1 23.04.2021/MyHeader.h
@@ -0,0 +1,40 @@
+#pragma once
+void Invitation();
+
+int GetRoll();
+
+void DrawDie1(int x, int y);
+
+void DrawDie2(int x, int y);
+
+void Roll2(int x, int y);
+
+void Roll3(int x, int y);
+
+void Roll4(int x, int y);
+
+void Roll5(int x, int y);
+
+void Roll6(int x, int y);
+
+void Roll7(int x, int y);
+
+void Roll8(int x, int y);
+
+void Roll9(int x, int y);
+
+void Roll10(int x, int y);
+
+void Roll11(int x, int y);
+
+void Roll12(int x, int y);
+
+void PrintMyTurn();
+
+void WriteHorz(int x, int y, char ch, unsigned int length);
+
+void WriteCorners(int x, int y);
+
+void PrintResult(unsigned roll1, unsigned roll2);
+
+void PressEnter();
