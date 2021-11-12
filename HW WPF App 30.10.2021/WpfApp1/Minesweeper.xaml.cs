889ebf4cb6f57a5768c2bf52def345f06f6ba505
D-O-A <79813608+D-O-A@users.noreply.github.com>
2021-11-13 01:21:01 +0200

    Updated minesweeper task

--- HW WPF App 30.10.2021/WpfApp1/Minesweeper.xaml.cs
+++ HW WPF App 30.10.2021/WpfApp1/Minesweeper.xaml.cs
@@ -11,71 +11,136 @@ namespace WpfApp1
     /// </summary>
     public partial class Minesweeper : Window
     {
+        private readonly Random random;
+
         public Minesweeper()
         {
             InitializeComponent();
-            for (int x = 0; x < App.SizeX; x++)
+            random = new Random();
+            //SystemSounds.Exclamation.Play();
+
+            for (int y = 0; y < App.SizeY; y++)
             {
-                for (int y = 0; y < App.SizeY; y++)
+                for (int x = 0; x < App.SizeX; x++)
                 {
                     var mineLabel = new MineLabel
                     {
                         X = x,
                         Y = y,
-                        Content = "\x224b",
                         FontSize = 30,
                         VerticalContentAlignment = VerticalAlignment.Center,
                         HorizontalContentAlignment = HorizontalAlignment.Center
                     };
                     mineLabel.MouseDown += MineLabel_MouseDown;
+                    this.RegisterName("label_" + x + "_" + y, mineLabel);
                     Field.Children.Add(mineLabel);
                 }
             }
+            Restart();
         }
 
         private void MineLabel_MouseDown(object sender, MouseButtonEventArgs e)
         {
             var mineLabel = sender as MineLabel;
-            var r = new Random();
 
             if (mineLabel == null)
             {
                 return;
             }
 
-            mineLabel.Content = r.Next(0, 9);
+            //если клик на мину - конец
+            if (mineLabel.IsMine)
+            {
+                if (MessageBoxResult.Yes == MessageBox.Show("Еще раз?", "Бух!", MessageBoxButton.YesNo))
+                {
+                    Restart();
+                    return;
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
 
-            switch (mineLabel.Content)
+            //определяем соседей
+            int x = mineLabel.X, y = mineLabel.Y;
+            //массив предполагаемых имен:
+            string[] names =
+            {
+                "label_" + (x-1) + "_" + (y-1),
+                "label_" + (x-1) + "_" + (y),
+                "label_" + (x-1) + "_" + (y+1),
+                "label_" + (x)   + "_" + (y-1),
+                "label_" + (x)   + "_" + (y+1),
+                "label_" + (x+1) + "_" + (y-1),
+                "label_" + (x+1) + "_" + (y),
+                "label_" + (x+1) + "_" + (y+1)
+            };
+            //счетчик мин
+            int mines = 0;
+            //цикл по именам
+            foreach (string name in names)
+            {
+                // Ищем по имени (ссылку на Label)
+                MineLabel label = this.FindName(name) as MineLabel;
+                if (label != null) //такое имя найдено
+                {
+                    // Проверям или это мина
+                    if (label.IsMine)
+                    {
+                        //увеличиваем счетчик
+                        mines++;
+                    }
+                }
+            }
+            mineLabel.Content = mines.ToString();
+
+            switch (mines)
             {
                 case 0:
-                    mineLabel.Background = Brushes.Crimson;
+                    mineLabel.Foreground = Brushes.Crimson;
                     break;
                 case 1:
-                    mineLabel.Background = Brushes.Orange;
+                    mineLabel.Foreground = Brushes.Orange;
                     break;
                 case 2:
-                    mineLabel.Background = Brushes.GreenYellow;
+                    mineLabel.Foreground = Brushes.GreenYellow;
                     break;
                 case 3:
-                    mineLabel.Background = Brushes.DarkGreen;
+                    mineLabel.Foreground = Brushes.DarkGreen;
                     break;
                 case 4:
-                    mineLabel.Background = Brushes.DarkBlue;
+                    mineLabel.Foreground = Brushes.DarkBlue;
                     break;
                 case 5:
-                    mineLabel.Background = Brushes.DeepSkyBlue;
+                    mineLabel.Foreground = Brushes.DeepSkyBlue;
                     break;
                 case 6:
-                    mineLabel.Background = Brushes.BlueViolet;
+                    mineLabel.Foreground = Brushes.BlueViolet;
                     break;
                 case 7:
-                    mineLabel.Background = Brushes.DeepPink;
+                    mineLabel.Foreground = Brushes.DeepPink;
                     break;
                 case 8:
-                    mineLabel.Background = Brushes.Brown;
+                    mineLabel.Foreground = Brushes.Brown;
                     break;
             }
-            //MessageBox.Show("Click " + mineLabel.X + " " + mineLabel.Y);
+        }
+
+        private void Restart()
+        {
+            foreach (var child in Field.Children)
+            {
+                MineLabel label = child as MineLabel;
+                if (label != null)
+                {
+                    bool isMine = random.Next(5) == 0;
+                    label.IsMine = isMine;
+                    label.Content = "\x224b";
+                    label.Foreground = Brushes.Black;
+                    label.labelState = LabelState.Unvisited;
+                }
+            }
         }
     }
 
@@ -83,5 +148,15 @@ namespace WpfApp1
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public bool IsMine { get; set; }
+
+        public LabelState labelState;
+    }
+
+    enum LabelState
+    {
+        Unvisited,
+        Flagged,
+        Open
     }
 }
