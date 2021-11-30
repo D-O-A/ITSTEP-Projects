6b74cbb7951f76274b7315f6a361ad27efdaf364
D-O-A <79813608+D-O-A@users.noreply.github.com>
2021-11-30 20:08:56 +0200

    Added Styles task

---
+++ HW Styles WPF 30.11.2021/StylesWPF/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace StylesWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ HW Styles WPF 30.11.2021/StylesWPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace StylesWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
