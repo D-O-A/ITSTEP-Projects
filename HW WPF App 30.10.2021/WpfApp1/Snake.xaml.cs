491c87fe6f3efb919a879432556c0820c256a709
D-O-A <79813608+D-O-A@users.noreply.github.com>
2021-12-03 20:49:56 +0200

    Updated snake task

--- HW WPF App 30.10.2021/WpfApp1/Snake.xaml.cs
+++ HW WPF App 30.10.2021/WpfApp1/Snake.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -12,10 +17,23 @@ namespace WpfApp1
     public partial class Snake : Window
     {
         private readonly List<Segment> Python;
+        private DispatcherTimer timer;
+        private MoveDirection moveDirection;
+        private Random r;
 
         public Snake()
         {
             InitializeComponent();
+
+            r = new Random();
+
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(300)
+            };
+            //timer.Tick += TimerTickBW;
+            timer.Tick += TimerTickColor;
+
             Python = new List<Segment>();
             for (int i = 0; i < 5; i++)
             {
@@ -25,22 +43,129 @@ namespace WpfApp1
                     {
                         Width = 20,
                         Height = 20,
-                        Fill = Brushes.Turquoise
+                        Fill = new SolidColorBrush(Color.FromRgb((byte)r.Next(0, 250), (byte)r.Next(0, 250), (byte)r.Next(0, 250)))/*Brushes.Turquoise*/
                     },
-                    X = 100 + i * 10,
+                    X = 400 + i * 20,
                     Y = 100
                 });
 
             }
         }
 
+        // 1. (Простое решение) Хвостовой сегмент перенести вперед
+        private void TimerTickBW(object sender, EventArgs e)
+        {
+            Segment tail = Python.First(); // ссылка на 1й элемент 
+            Segment head = Python.Last(); // ссылка на последний элемент
+            Python.Remove(tail);
+
+            switch (moveDirection)
+            {
+                case MoveDirection.Left:
+                    tail.X = head.X - tail.Figure.Width;
+                    tail.Y = head.Y;
+                    break;
+                case MoveDirection.Right:
+                    tail.X = head.X + tail.Figure.Width;
+                    tail.Y = head.Y;
+                    break;
+                case MoveDirection.Up:
+                    tail.X = head.X;
+                    tail.Y = head.Y - tail.Figure.Height;
+                    break;
+                case MoveDirection.Down:
+                    tail.X = head.X;
+                    tail.Y = head.Y + tail.Figure.Height;
+                    break;
+            }
+
+            Python.Add(tail);
+            tail.Show(Field);
+        }
+
+
+        private void TimerTickColor(object sender, EventArgs e)
+        {
+            for (int i = Python.Count - 1; i >= 1; i--)
+            {
+                Segment seg = Python[i];
+                Segment seg_1 = Python[i - 1];
+                seg.X = seg_1.X;
+                seg.Y = seg_1.Y;
+            }
+
+            switch (moveDirection)
+            {
+                case MoveDirection.Left:
+                    Python[0].X -= 20;
+                    break;
+                case MoveDirection.Right:
+                    Python[0].X += 20;
+                    break;
+                case MoveDirection.Up:
+                    Python[0].Y -= 20;
+                    break;
+                case MoveDirection.Down:
+                    Python[0].Y += 20;
+                    break;
+            }
+
+            ShowPython();
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowPython();
+            moveDirection = MoveDirection.Left;
+            timer.Start();
+        }
+
+        private void ShowPython()
         {
             foreach (var segment in Python)
             {
                 segment.Show(Field);
             }
         }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    if (moveDirection != MoveDirection.Right)
+                    {
+                        moveDirection = MoveDirection.Left;
+                    }
+                    break;
+                case Key.Right:
+                    if (moveDirection != MoveDirection.Left)
+                    {
+                        moveDirection = MoveDirection.Right;
+                    }
+                    break;
+                case Key.Up:
+                    if (moveDirection != MoveDirection.Down)
+                    {
+                        moveDirection = MoveDirection.Up;
+                    }
+                    break;
+                case Key.Down:
+                    if (moveDirection != MoveDirection.Up)
+                    {
+                        moveDirection = MoveDirection.Down;
+                    }
+                    break;
+            }
+        }
+    }
+
+    public enum MoveDirection
+    {
+        Left,
+        Right,
+        Up,
+        Down
     }
 
     public class Segment
@@ -65,14 +190,15 @@ namespace WpfApp1
 }
 
 
+
 /*
  * Силы связи: (от сильной к слабой)
  * Наследование (обобщение)
- * Композиция
- * Агрегация
- * Ассоциация
- * Зависимость
+ * Композиция - объект для своей жизни создает другой объект, жизненный цикл зависит от включенного объекта
+ * Агрегация - жизненный цикл не зависит от включенного объекта
+ * Ассоциация - В одном объекте есть переменная или поле типа другого объекта.
+ * Зависимость - Слабая зависимость, потому что всегда можно "легко" переписать
  * 
  * 
  * 
- * 
+ */
