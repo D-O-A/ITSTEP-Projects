616324f67c19768ef7f95244da54201ad67d1c40
D-O-A <79813608+D-O-A@users.noreply.github.com>
2022-01-26 00:46:31 +0200

    Added ADO.NET task

---
+++ HW ADO_NET 24.01.2022/ADO_NET/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace ADO_NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(27, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect DB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(262, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(196, 274);
+            this.listBox1.TabIndex = 1;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCreate.Location = new System.Drawing.Point(27, 108);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(160, 37);
+            this.buttonCreate.TabIndex = 2;
+            this.buttonCreate.Text = "Create table";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonInsert.Location = new System.Drawing.Point(27, 162);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(160, 42);
+            this.buttonInsert.TabIndex = 3;
+            this.buttonInsert.Text = "Insert Line";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonDisconnect.Location = new System.Drawing.Point(27, 223);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(160, 41);
+            this.buttonDisconnect.TabIndex = 4;
+            this.buttonDisconnect.Text = "Disconnect DB";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonDisconnect;
+    }
+}
+
---
+++ HW ADO_NET 24.01.2022/ADO_NET/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ADO_NET
+{
+    public partial class Form1 : Form
+    {
+        private readonly SqlConnection _connection;
+        private readonly string _tableName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _connection = new SqlConnection(
+                @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=J:\GitHub\HW ADO_NET 24.01.2022\ADO_NET\Database1.mdf;Integrated Security=True"
+            // - Desktop DB
+            //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="J:\GitHub\HW ADO_NET 24.01.2022\ADO_NET\Database1.mdf";Integrated Security=True
+            );
+
+            _tableName = "Nums";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (SqlConnectionChecker())
+                {
+                    MessageBox.Show(@"Connection is already opened");
+                    return;
+                }
+
+                _connection.Open();
+                listBox1.Items.Add("Connected");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            if (!SqlConnectionChecker())
+            {
+                MessageBox.Show(@"Open connection first");
+                return;
+            }
+
+            if (IsTableExists())
+            {
+                return;
+            }
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = _connection;
+            // создание таблицы
+            cmd.CommandText = $@"
+                CREATE TABLE {_tableName}(
+                  id  UNIQUEIDENTIFIER PRIMARY KEY,
+                  num INT
+                ) ";
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                listBox1.Items.Add("CREATE TABLE Ok");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            if (!SqlConnectionChecker())
+            {
+                MessageBox.Show(@"Open connection first");
+                return;
+            }
+
+            using SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = _connection;
+            cmd.CommandText = @$"INSERT INTO {_tableName}"
+                              + " VALUES( NEWID(), "
+                              + DateTime.Now.Millisecond
+                              + ")";
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                listBox1.Items.Add("INSERT Ok");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!SqlConnectionChecker())
+                {
+                    MessageBox.Show(@"Connection is already closed");
+                    return;
+                }
+
+                _connection.Close();
+                listBox1.Items.Add("Disconnected");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private bool SqlConnectionChecker()
+        {
+            return _connection.State is ConnectionState.Open; //best practices от Microsoft'а подъехали))
+        }
+
+        private bool IsTableExists()
+        {
+            //не хватило мне мозгов придумать красивее решение
+
+            SqlCommand cmd = new SqlCommand();
+            bool tableExists = true;
+
+            cmd.Connection = _connection;
+            cmd.CommandText = $@"SELECT COUNT(*) FROM {_tableName}";
+
+            try
+            {
+                cmd.ExecuteScalar();
+            }
+            catch (SqlException)
+            {
+                tableExists = false;
+            }
+
+            return tableExists;
+        }
+    }
+}
